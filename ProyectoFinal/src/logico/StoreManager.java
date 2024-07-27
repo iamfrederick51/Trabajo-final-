7c22d3effb8ef2e30118c884179de06da191dcbe
Imfrederick51 <125817179+iamfrederick51@users.noreply.github.com>
2024-07-27 16:18:02 -0400

    Update StoreManager.java

--- ProyectoFinal/src/logico/StoreManager.java
+++ ProyectoFinal/src/logico/StoreManager.java
@@ -1,99 +1,25 @@
 package logico;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
+public class SessionManager {
+    private static User currentUser;
 
-public class StoreManager {
-    private List<Component> components;
-    private List<Customer> customers;
-    private List<Sale> sales;
-
-    public StoreManager() {
-        components = new ArrayList<>();
-        customers = new ArrayList<>();
-        sales = new ArrayList<>();
+    public static void login(User user) {
+        currentUser = user;
     }
 
-    public void addComponent(Component component) {
-        components.add(component);
+    public static void logout() {
+        currentUser = null;
     }
 
-    public void updateComponent(Component component) {
-        // Implementar lógica de actualización
+    public static User getCurrentUser() {
+        return currentUser;
     }
 
-    public void removeComponent(int id) {
-        components.removeIf(component -> component.getId() == id);
-    }
-    
-    public void saveComponents() {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("component.dat"))) {
-            oos.writeObject(components);
-            oos.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static boolean isAuthenticated() {
+        return currentUser != null;
     }
 
-    public void loadComponents() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("component.dat"))) {
-            components = (List<Component>) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    public static boolean isAdmin() {
+        return currentUser != null && "Administrador".equals(currentUser.getRole());
     }
-
-    public List<Component> getComponents() {
-        return components;
-    }
-
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
-
-    public void updateCustomer(Customer customer) {
-        // Implementar lógica de actualización
-    }
-
-    public void removeCustomer(int id) {
-        customers.removeIf(customer -> customer.getId() == id);
-    }
-
-    public List<Customer> getCustomers() {
-        return customers;
-    }
-
-    public void addSale(Sale sale) {
-        sales.add(sale);
-    }
-
-    public List<Sale> getSales() {
-        return sales;
-    }
-    public void removeSale(Sale sale) {
-        sales.remove(sale);
-    }
-//Método para guardar clientes en un archivo
-public void saveCustomers(String filename) {
-    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-        oos.writeObject(customers);
-        oos.flush();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
-
-// Método para cargar clientes desde un archivo
-public void loadCustomers(String filename) {
-    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-        customers = (List<Customer>) ois.readObject();
-    } catch (IOException | ClassNotFoundException e) {
-        e.printStackTrace();
-    }
-}
 }
